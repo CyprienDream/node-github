613178d9a7f9b3c271d7af2e293ac4a63d15f180
Cyprien Eckert <cipri.eckert@hotmail.fr>
2022-01-08 17:49:03 +0100

    add most used languages to user component

--- src/UserComponent.tsx
+++ src/UserComponent.tsx
@@ -8,8 +8,9 @@ const UserComponent = (props: { user: IUser }) => {
         <img src={'' + user.avatar_url} alt="" className="user-picture" />
       </div>
       <div className="user-info">
-        <a href={"https://api.github.com/users/" + user.login} target="_bla" className="user-link">{user.name}</a>
+        <a href={"https://github.com/" + user.login} target="_bla" className="user-link">{user.name}</a>
         <p>{user.bio}</p>
+        <p><img src={"https://github-readme-stats.vercel.app/api/top-langs?username=" + user.login + "&show_icons=true&locale=en&layout=compact"} alt="" /></p>
         <p>Public repos : {user.public_repos}</p>
       </div>
     </div>
