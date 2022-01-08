0a5b22fee89424dbc42947b5c849f3f1bef9d3dd
Cyprien Eckert <cipri.eckert@hotmail.fr>
2022-01-08 17:37:28 +0100

    add footer component

---
+++ src/FooterComponent.tsx
@@ -0,0 +1,16 @@
+
+const FooterComponent = () => {
+  return (
+
+    <div className="footer">
+      <div className="footer-links">
+        <a href="https://github.com/CyprienDream"><i className="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/cyprieneckert/"><i className="fab fa-linkedin-in"></i></a>
+      </div>
+
+    </div>
+
+  )
+};
+
+export default FooterComponent;
