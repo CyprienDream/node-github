01c41735e72cc794ca7f01b3e6804d23a5fac8c0
Cyprien Eckert <cipri.eckert@hotmail.fr>
2022-01-08 13:55:34 +0100

    add: repository component

---
+++ src/RepositoryComponent.tsx
@@ -0,0 +1,21 @@
+import { IRepository } from "./IRepository";
+
+const RepositoryComponent = (props: { repo: IRepository }) => {
+  const { repo } = props;
+  return (
+
+
+    <div className="repo">
+      <a href={repo.clone_url} className="card-link">
+        <div className="card-body">
+          <h3 className="card-title">{repo.name}</h3>
+          <p className="card-text">{repo.description}</p>
+          <p>Stars : {repo.watchers_count}</p>
+        </div>
+      </a>
+    </div>
+
+  )
+};
+
+export default RepositoryComponent;
