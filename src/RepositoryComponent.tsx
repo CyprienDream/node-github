2029543a61e84bdef260e3519c8e4972771232a6
Cyprien Eckert <cipri.eckert@hotmail.fr>
2022-01-08 19:36:34 +0100

    add flex property to repo card

--- src/RepositoryComponent.tsx
+++ src/RepositoryComponent.tsx
@@ -7,11 +7,13 @@ const RepositoryComponent = (props: { repo: IRepository }) => {
     <div className="repo">
       <a href={repo.clone_url} className="card-link">
         <div className="card-body">
-          <h3 className="card-title">{repo.name}</h3>
-          <p className="card-text">{repo.description}</p>
+          <div className="repo-info">
+            <h3 className="card-title">{repo.name}</h3>
+            <p className="card-text">{repo.description}</p>
+          </div>
+
           <div className="favourites">
             <p>{repo.language} - {repo.watchers_count} <i className="fas fa-star"></i></p>
-
           </div>
         </div>
       </a>
