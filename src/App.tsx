007c41c28ab17106673c585124d8167999a9d01e
Cyprien Eckert <cipri.eckert@hotmail.fr>
2022-01-09 17:00:04 +0100

    refactor + doc app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,13 @@ import './App.css';
 
 function App() {
 
+  // defines a set of variables and their setters useful for storing api and user data
   const [reposFound, setReposFound] = useState<IRepository[]>([]);
   const [usernameInput, setUsernameInput] = useState('');
   const [repositoryInput, setRepositoryInput] = useState('');
   const [user, setUser] = useState<IUser>();
 
+  // renders user data if the request was successful and an informative message otherwise
   const foundInfo = () => {
     if (!usernameInput.length && !reposFound.length) return <p>Please enter a GitHub username</p>;
     if (!reposFound.length && usernameInput.length > 0) return <p>No results for {usernameInput}.</p>;
@@ -28,23 +30,32 @@ function App() {
     )
   }
 
+  // displays repository components with user filtering functionality
+  const displayRepos = () => {
+    return (
+    <div className="repos-container">
+      {(reposFound.length || '') &&
+        reposFound.filter(val => {
+          // return all elements if there is no filtering input
+          if (repositoryInput === '') {
+            return val
+            // return value if it matches filtering input
+          } else if (val.name.toLowerCase().includes(repositoryInput.toLowerCase())) {
+            return val
+          }
+        }).map(repo =>
+          (<RepositoryComponent key={repo.clone_url} repo={repo}></RepositoryComponent>))
+      }
+    </div>
+    )
+  }
+
   return (
     <div className="App">
       <HeaderComponent  setReposFound={setReposFound} setUsernameInput={setUsernameInput} setUser={setUser}></HeaderComponent>
       <div className="main">
         {foundInfo()}
-        <div className="repos-container">
-          {(reposFound.length || '') &&
-            reposFound.filter(val => {
-              if (repositoryInput === '') {
-                return val
-              } else if (val.name.toLowerCase().includes(repositoryInput.toLowerCase())) {
-                return val
-              }
-            }).map(repo =>
-                (<RepositoryComponent key={repo.clone_url} repo={repo}></RepositoryComponent>))
-          }
-        </div>
+        {displayRepos()}
       </div>
       <FooterComponent></FooterComponent>
     </div>
